1d2aafd4fedd020d30c0a28c6259edd2617b958a
dominhkha <kha.minhdo199@gmail.com>
2021-07-20 00:43:15 +0700

    run cw

--- setup_mnist.py
+++ setup_mnist.py
@@ -113,9 +113,9 @@ class MNISTModel:
         # model.add(Dropout(0.5))
         # model.add(Dense(10, activation="relu"))
         # model.load_weights(restore)
-        self.model = tf.keras.models.load_model('models/Alexnet.h5', compile=False)
+        self.model = tf.keras.models.load_model('models/Lenet_v2.h5', compile=False)
         self.model = tf.keras.models.Model(inputs=self.model.input,
-                                                   outputs=self.model.get_layer('dense_3').output)
+                                                   outputs=self.model.get_layer('pre_softmax_layer').output)
 
     def predict(self, data):
         return self.model(data)
