fddd89b36659943e62ce84b57367e0892efd1a72
dominhkha <kha.minhdo199@gmail.com>
2021-08-12 20:57:42 +0700

    run cw cifar alexnet

--- test_attack.py
+++ test_attack.py
@@ -48,7 +48,7 @@ def generate_data(data, samples, targeted=True, start=0, inception=False):
             #     seq = random.sample(range(1,1001), 10)
             # else:
             #     seq = range(data.test_labels.shape[1])
-            seq = 4000
+            seq = 1000
             count = 0
             for j in range(0, data.train_data.shape[0]):
                 # if (j == np.argmax(data.test_labels[start+i])) and (inception == False):
@@ -95,7 +95,7 @@ if __name__ == "__main__":
         timeend = time.time()
 
         print("Took",timeend-timestart,"seconds to run",len(inputs),"samples.")
-        f = open('alexnet_cifar_4k.txt', 'w')
+        f = open('alexnet_cifar.txt', 'w')
         text = 'time: ' + str(timeend - timestart)
         if adv is not None:
             text += '\nsuccess_rate: ' + str(adv.shape[0])
